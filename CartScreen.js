98065f343ef8049a43a72501a23553770f205afd
Elyes2024-2023 <ilies.b.2024@protonmail.com>
2024-09-01 20:48:58 +0200

    Update CartScreen.js

--- CartScreen.js
+++ CartScreen.js
@@ -1,89 +1,95 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
-import Message from '../Message'
-import { addToCart,removeFromCart } from '../../actions/cartActions'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
+import Message from '../Message';
+import { addToCart, removeFromCart } from '../../actions/cartActions';
 
+/**
+ * CartScreen component displays the shopping cart with options to modify item quantities
+ * and proceed to checkout.
+ *
+ * @param {object} props - Component props including match, location, and history.
+ */
 function CartScreen({ match, location, history }) {
-    const productId = match.params.id
-    const qty = location.search ? Number(location.search.split('=')[1]) : 1
-    const dispatch = useDispatch()
+    // Extract productId from URL params and quantity from query string
+    const productId = match.params.id;
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1;
 
-    const cart = useSelector(state => state.cart)
-    const { cartItems } = cart
+    // Use dispatch to send actions to the Redux store
+    const dispatch = useDispatch();
 
+    // Access cart state from the Redux store
+    const cart = useSelector(state => state.cart);
+    const { cartItems } = cart;
+
+    // Effect to add the item to the cart when productId or qty changes
     useEffect(() => {
         if (productId) {
-            dispatch(addToCart(productId, qty))
+            dispatch(addToCart(productId, qty));
         }
-    }, [dispatch, productId, qty])
+    }, [dispatch, productId, qty]);
 
+    // Handler to remove an item from the cart
     const removeFromCartHandler = (id) => {
-        dispatch(removeFromCart(id))
-    }
-    const checkoutHandler = () => {
-        history.push('/login?redirect=shipping')
-    }
+        dispatch(removeFromCart(id));
+    };
 
+    // Handler to proceed to checkout
+    const checkoutHandler = () => {
+        history.push('/login?redirect=shipping');
+    };
 
     return (
         <Row>
             <Col md={8}>
                 <h1>Shopping Cart</h1>
                 {cartItems.length === 0 ? (
+                    // Display a message if the cart is empty
                     <Message variant='info'>
                         Your cart is empty <Link to='/'>Go Back</Link>
                     </Message>
                 ) : (
-                        <ListGroup variant='flush'>
-                            {cartItems.map(item => (
-                                <ListGroup.Item key={item.product}>
-                                    <Row>
-                                        <Col md={2}>
-                                            <Image src={item.image} alt={item.name} fluid rounded />
-                                        </Col>
-                                        <Col md={3}>
-                                            <Link to={`/product/${item.product}`}>{item.name}</Link>
-                                        </Col>
-
-                                        <Col md={2}>
-                                            ${item.price}
-                                        </Col>
-
-                                        <Col md={3}>
-                                            <Form.Control
-                                                as="select"
-                                                value={item.qty}
-                                                onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}
-                                             
-                                            >
-                                                {
-
-                                                    [...Array(item.countInStock).keys()].map((x) => (
-                                                        <option key={x + 1} value={x + 1}>
-                                                            {x + 1}
-                                                        </option>
-                                                    ))
-                                                }
-
-                                            </Form.Control>
-                                        </Col>
-
-                                        <Col md={1}>
-                                            <Button
-                                                type='button'
-                                                variant='light'
-                                                onClick={() => removeFromCartHandler(item.product)}
-                                            >
-                                                <i className='fas fa-trash'></i>
-                                            </Button>
-                                        </Col>
-                                    </Row>
-                                </ListGroup.Item>
-                            ))}
-                        </ListGroup>
-                    )}
+                    <ListGroup variant='flush'>
+                        {cartItems.map(item => (
+                            <ListGroup.Item key={item.product}>
+                                <Row>
+                                    <Col md={2}>
+                                        <Image src={item.image} alt={item.name} fluid rounded />
+                                    </Col>
+                                    <Col md={3}>
+                                        <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                    </Col>
+                                    <Col md={2}>
+                                        ${item.price}
+                                    </Col>
+                                    <Col md={3}>
+                                        <Form.Control
+                                            as="select"
+                                            value={item.qty}
+                                            onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}
+                                        >
+                                            {[...Array(item.countInStock).keys()].map((x) => (
+                                                <option key={x + 1} value={x + 1}>
+                                                    {x + 1}
+                                                </option>
+                                            ))}
+                                        </Form.Control>
+                                    </Col>
+                                    <Col md={1}>
+                                        <Button
+                                            type='button'
+                                            variant='light'
+                                            onClick={() => removeFromCartHandler(item.product)}
+                                        >
+                                            <i className='fas fa-trash'></i>
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                )}
             </Col>
 
             <Col md={4}>
@@ -105,12 +111,10 @@ function CartScreen({ match, location, history }) {
                             Proceed To Checkout
                         </Button>
                     </ListGroup.Item>
-
-
                 </Card>
             </Col>
         </Row>
-    )
+    );
 }
 
-export default CartScreen
+export default CartScreen;
