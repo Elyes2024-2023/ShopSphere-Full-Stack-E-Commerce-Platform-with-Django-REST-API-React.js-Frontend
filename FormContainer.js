866f08426e4169e89f55d4e249a5ff22fec22b4d
Elyes2024-2023 <ilies.b.2024@protonmail.com>
2024-09-01 20:56:23 +0200

    Update FormContainer.js

--- FormContainer.js
+++ FormContainer.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import {Container,Row,Col} from 'react-bootstrap'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-
-function FormContainer({children}) {
-    return (
-        <div>
-            <Container>
-                <Row className="justify-content-md-center">
-
-                    <Col xs={12} md={6}>
-                    {children}
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    )
+/**
+ * A container component to center and format form content.
+ * 
+ * This component uses Bootstrap's grid system to center its children 
+ * in a column with a medium breakpoint of 6 columns width.
+ *
+ * @param {Object} props - The component props.
+ * @param {React.ReactNode} props.children - The content to be displayed inside the container.
+ * @returns {JSX.Element} - The FormContainer component.
+ */
+function FormContainer({ children }) {
+  return (
+    <div className="form-container">
+      <Container>
+        <Row className="justify-content-md-center">
+          {/* Centered column that holds the form content */}
+          <Col xs={12} md={6}>
+            {children}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default FormContainer
+export default FormContainer;
