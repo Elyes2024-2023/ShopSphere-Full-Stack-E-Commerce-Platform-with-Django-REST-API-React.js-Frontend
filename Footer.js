c11278353b460c83fb6b3b7134ab87db23138e4b
Elyes2024-2023 <ilies.b.2024@protonmail.com>
2024-09-01 20:55:14 +0200

    Update Footer.js

--- Footer.js
+++ Footer.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
+/**
+ * Footer component that displays the footer section of the website.
+ * It includes a container with a centered copyright message.
+ *
+ * @returns {JSX.Element} - The Footer component.
+ */
 function Footer() {
   return (
-    <footer>
+    <footer className="bg-dark text-light">
       <Container>
         <Row>
-          <Col className="text-center  py-3">Copyrights @copy ARKPROCODER</Col>
+          {/* Center-aligned column containing the copyright text */}
+          <Col className="text-center py-3">
+            &copy; {new Date().getFullYear()} ARKPROCODER
+          </Col>
         </Row>
       </Container>
     </footer>
